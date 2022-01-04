f46100f4b0958dea7f20cc35cdb50d58eaae2e37
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-05 00:33:06 +0200

    add new projects

---
+++ Functions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sisesta esimene arv:");
+            int userNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine arv");
+            int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Palun vali tehe ('+', '-', '*' voi '/'):");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '/':
+                    DivideTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '*':
+                    DoubleTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '-':
+                    SubtractTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                default:
+                    Console.WriteLine("Kena paeva!");
+                    break;
+
+            }
+
+
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(result);
+        }
+        public static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine(result);
+        }
+
+        public static void SubtractTwoNumbers(int x, int y)
+
+        {
+            int result = x - y;
+            Console.WriteLine(result);
+        }
+
+        public static void DoubleTwoNumbers(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine(result);
+        }
+    }
+}
