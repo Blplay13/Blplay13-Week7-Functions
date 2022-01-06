0a1e3d2531136d5d99d54bb0ad778a25b29aa6aa
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:47:16 +0200

    Update Program.cs

--- UserNameData/Program.cs
+++ UserNameData/Program.cs
@@ -10,6 +10,28 @@ namespace UserNameData
     {
         static void Main(string[] args)
         {
+             //programm kusib kasutajal sesestada oma eesnime;
+            //programm kuvab kasutaja eesnime pikkust;
+            //programm kuvab kasutaja eesnime esimest tahte;
+            //programm kuvab kasutaja eesnime tagurpidi;
+            //main meetodis ei tohi olla rohkem kui kolm rida koodi
+
+            Console.WriteLine("Palun sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+
+            GetUserNameData(userName);
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sumbolit pikk");
+            Console.WriteLine($"Sinu nime esimene taht on {userInput[0]}");
+
+            for(int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(userInput[i]);
+            }
+            
         }
     }
 }
