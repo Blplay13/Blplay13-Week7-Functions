0eb01942d734586fcdbe86d960fd60970ba22c43
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:48:17 +0200

    Update Program.cs

--- UserNameDataTask/Program.cs
+++ UserNameDataTask/Program.cs
@@ -10,6 +10,53 @@ namespace UserNameDataTask
     {
         static void Main(string[] args)
         {
+             //programm kusib kasutajal sisestada eesnimi;
+            //programm kusib kasutajal sisestada numbrit 1-3;
+            //kui kasutaja sisestab 1, siis kuvatakse kasutaja eesnime tagurpidi;
+            //kui kasutaja sisestab 2, siis kuvatakse kasutaja eesnime esimest tahte;
+            //kui kasutaja valib 3, siis kuvatakse kasitaja eesnime pikkust;
+
+            Console.WriteLine("Palun sisestage oma eesnimi:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Palun sisestage number 1 kuni 3:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+
+            {
+                case 1:
+                    Reverse(userName);
+                    break;
+                case 2:
+                    FirstLetter(userName);
+                    break;
+                case 3:
+                    nameLength(userName);
+                    break;
+                default:
+                    Console.WriteLine("Kena paeva!");
+                    break;
+
+            }
         }
+
+        public static void Reverse(string userName)
+        {
+            for(int i = userName.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(userName[i]);
+            }
+        }
+
+        public static void FirstLetter(string userInput)
+        {
+            Console.WriteLine($"Sinu nime esimene taht on {userInput[0]}");
+        }
+        public static void nameLength(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sumbolit pikk");
+        }
+        
     }
 }
